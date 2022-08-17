f6ce5e07acea0270a6bca3a6492af908b6b941be
edurevsky <edurevsky@gmail.com>
2022-08-17 10:12:50 -0300

    ...

--- src/pages/AppPage/index.tsx
+++ src/pages/AppPage/index.tsx
@@ -1,14 +1,10 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { AuthContext } from "../../contexts/AuthContext/auth-context";
 import ContactsPage from "../ContactsPage";
 import LoginPage from "../LoginPage";
 
 const AppPage = () => {
   const { token } = useContext(AuthContext);
-
-  useState(() => {
-
-  });
 
   return (
     <>
